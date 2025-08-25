8945882eefa05132df54003cf5bee59b3cc00887
muxx31 <sanjaywrs07@gmail.com>
2025-08-25 23:58:01 +0530

    fixed url

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,4 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://campuswellness-1.onrender.com';
+//const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://campuswellness-1.onrender.com';
+//export default BACKEND_URL;
+const BACKEND_URL = 'https://campuswellness-1.onrender.com';
 export default BACKEND_URL;
